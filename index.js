e8bd10aa809c7c1460490905ae4ecd26058791af
Jeremiah Donley <108106416+WeiQiPro@users.noreply.github.com>
2024-05-17 21:32:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,233 +1,200 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const grid = document.querySelector(".grid");
-  const totalScorePointsSpan = document.getElementById("total");
-  const buttons = document.querySelectorAll(".keyboard button");
-
-  const PD = {
-    A: 5,
-    E: 5,
-    L: 5,
-    N: 5,
-    R: 5,
-    S: 5,
-    T: 5,
-    H: 4,
-    I: 4,
-    O: 4,
-    C: 3,
-    D: 3,
-    P: 3,
-    U: 3,
-    Y: 3,
-    B: 2,
-    W: 2,
-    F: 2,
-    G: 2,
-    J: 2,
-    K: 2,
-    M: 2,
-    Q: 1,
-    V: 1,
-    X: 1,
-    Z: 1,
-  };
-
-  const RD = 10; // round index multiplier
-
-  const wordLength = 5;
-  const maxGuesses = 6;
-  const currentYear = new Date().getFullYear();
-  const seed = currentYear;
-
-  let wordOfTheDay = "";
-  let currentGuess = "";
-  let round = 0;
-  let total = 0;
-  let firstWordPoints = 0;
-  let acceptableWords = new Set();
-
-  // Load the word lists
-  let wordleLA = [];
-  let wordleTA = [];
-
-  fetch(
-    "https://gist.githubusercontent.com/scholtes/94f3c0303ba6a7768b47583aff36654d/raw/d9cddf5e16140df9e14f19c2de76a0ef36fd2748/wordle-La.txt"
-  )
-    .then((response) => response.text())
-    .then((data) => {
-      wordleLA = data.split("\n").filter((word) => word.length === wordLength);
-      wordleLA.forEach((word) =>
-        acceptableWords.add(word.trim().toUpperCase())
-      );
-      wordOfTheDay = getWordOfTheDay();
-      wordOfTheDay = wordOfTheDay.toLocaleUpperCase(); // Assign the result back to wordOfTheDay
-    });
-
-  fetch(
-    "https://gist.githubusercontent.com/scholtes/94f3c0303ba6a7768b47583aff36654d/raw/d9cddf5e16140df9e14f19c2de76a0ef36fd2748/wordle-Ta.txt"
-  )
-    .then((response) => response.text())
-    .then((data) => {
-      wordleTA = data.split("\n").filter((word) => word.length === wordLength);
-      wordleTA.forEach((word) =>
-        acceptableWords.add(word.trim().toUpperCase())
-      );
-    });
-
-  function getWordOfTheDay() {
-    const today = new Date();
-    const dayOfYear = Math.floor(
-      (today - new Date(today.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24
-    );
-    const uniqueIndices = generateUniqueIndices(
-      currentYear,
-      wordleLA.length,
-      365
-    );
-    return wordleLA[uniqueIndices[dayOfYear]];
-  }
-
-  function generateUniqueIndices(seed, length, count) {
-    const indices = new Set();
-    let i = 0;
-    while (indices.size < count) {
-      const index = Math.floor(
-        (Math.abs(Math.sin(seed + i) + Math.cos(seed + i)) * 100000) % length
-      );
-      indices.add(index);
-      i++;
-    }
-    return Array.from(indices);
-  }
-
-  function createGrid() {
-    for (let i = 0; i < wordLength * maxGuesses; i++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      grid.appendChild(cell);
-    }
-  }
-
-  function updateGrid() {
-    const cells = grid.querySelectorAll(".cell");
-    cells.forEach((cell, index) => {
-      const row = Math.floor(index / wordLength);
-      const col = index % wordLength;
-      if (currentGuess.length > col && round === row) {
-        cell.textContent = currentGuess[col];
-      } else if (row < round) {
-        // Keep the letters from previous guesses
-      } else {
-        cell.textContent = "";
-      }
-    });
-  }
-
-  function handleInput(letter) {
-    if (letter === "<<<") {
-      currentGuess = currentGuess.slice(0, -1);
-    } else if (letter === "Enter") {
-      if (currentGuess.length === wordLength) {
-        if (acceptableWords.has(currentGuess.toUpperCase())) {
-          checkGuess();
-          if (round === 0) {
-            firstWordPoints = calculateScore(currentGuess);
-          }
-          if (currentGuess.toUpperCase() === wordOfTheDay) {
-            total = firstWordPoints + round * RD;
-            alert(`Congratulations! You guessed the word: ${wordOfTheDay}. Your total score is: ${total}`);
-            return; // End the game
-          } else {
-            if (round < 5) {
-              total = firstWordPoints + round * RD;
-              round++;
-            } else {
-              total = firstWordPoints + 60;
-              alert(`Game Over! The correct word was: ${wordOfTheDay}. Your total score is: ${total}`);
-              return; // End the game
-            }
-          }
-          currentGuess = "";
-        } else {
-          alert("Not a valid word");
-        }
-      }
-    } else if (currentGuess.length < wordLength) {
-      currentGuess += letter;
-    }
-    updateGrid();
-    totalScorePointsSpan.textContent = total.toString().padStart(1, "0");
-  }
-  
-
-  function calculateScore(word) {
-    return word.split("").reduce((score, letter) => {
-      return score + (PD[letter.toUpperCase()] || 0);
-    }, 0);
-  }
-
-  function checkGuess() {
-    const cells = grid.querySelectorAll(".cell");
-    const guessLetters = currentGuess.split("");
-    const wordLetters = wordOfTheDay.split("");
-    const buttonColors = {}; // Object to store button colors
-
-    // First pass: check for correct positions (green)
-    guessLetters.forEach((letter, index) => {
-      const cellIndex = round * wordLength + index;
-      const cell = cells[cellIndex];
-
-      if (letter === wordLetters[index]) {
-        cell.style.backgroundColor = "green";
-        wordLetters[index] = null; // Remove the letter from word to avoid double counting
-        buttonColors[letter.toUpperCase()] = "green";
-      }
-    });
-
-    // Second pass: check for wrong positions (yellow)
-    guessLetters.forEach((letter, index) => {
-      const cellIndex = round * wordLength + index;
-      const cell = cells[cellIndex];
-
-      if (
-        wordLetters.includes(letter) &&
-        cell.style.backgroundColor !== "green"
-      ) {
-        cell.style.backgroundColor = "#dac316";
-        wordLetters[wordLetters.indexOf(letter)] = null; // Remove the letter from word to avoid double counting
-        if (!buttonColors[letter.toUpperCase()]) {
-          buttonColors[letter.toUpperCase()] = "#dac316";
-        }
-      } else {
-        if (
-          cell.style.backgroundColor !== "green" &&
-          cell.style.backgroundColor !== "#dac316"
-        ) {
-          cell.style.backgroundColor = "#636363";
-          if (!buttonColors[letter.toUpperCase()]) {
-            buttonColors[letter.toUpperCase()] = "#636363";
-          }
-        }
-      }
-    });
-
-    // Update button colors based on guesses
-    updateButtonColors(buttonColors);
-  }
-
-  function updateButtonColors(buttonColors) {
-    buttons.forEach((button) => {
-      const letter = button.textContent.toUpperCase();
-      if (buttonColors[letter]) {
-        button.style.backgroundColor = buttonColors[letter];
-      }
-    });
-  }
-
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => handleInput(button.textContent));
-  });
-
-  createGrid();
-  updateGrid();
+  new e().qweu9rhtgi();
 });
+class t {
+  constructor() {
+    (this.qaeiurthg = []), (this.qeiruyght = []);
+  }
+  async loadLists() {
+    await this.o9uibg9uiasr(
+      "https://gist.githubusercontent.com/scholtes/94f3c0303ba6a7768b47583aff36654d/raw/d9cddf5e16140df9e14f19c2de76a0ef36fd2748/wordle-La.txt",
+      this.qaeiurthg
+    ),
+      await this.o9uibg9uiasr(
+        "https://gist.githubusercontent.com/scholtes/94f3c0303ba6a7768b47583aff36654d/raw/d9cddf5e16140df9e14f19c2de76a0ef36fd2748/wordle-Ta.txt",
+        this.qeiruyght
+      );
+  }
+  async o9uibg9uiasr(t, e) {
+    const s = await fetch(t),
+      r = await s.text();
+    e.push(
+      ...r
+        .split("\n")
+        .filter((t) => 5 === t.length)
+        .map((t) => t.trim().toUpperCase())
+    );
+  }
+}
+class e {
+  constructor() {
+    (this.grid = document.querySelector(".grid")),
+      (this.totalScorePointsSpan = document.getElementById("total")),
+      (this.buttons = document.querySelectorAll(".keyboard button")),
+      (this.wordListLoader = new t()),
+      (this.PD = {
+        A: 5,
+        E: 5,
+        L: 5,
+        N: 5,
+        R: 5,
+        S: 5,
+        T: 5,
+        H: 4,
+        I: 4,
+        O: 4,
+        C: 3,
+        D: 3,
+        P: 3,
+        U: 3,
+        Y: 3,
+        B: 2,
+        W: 2,
+        F: 2,
+        G: 2,
+        J: 2,
+        K: 2,
+        M: 2,
+        Q: 1,
+        V: 1,
+        X: 1,
+        Z: 1,
+      }),
+      (this.RD = 10),
+      (this.wordLength = 5),
+      (this.maxGuesses = 6),
+      (this.currentYear = new Date().getFullYear()),
+      (this.wordOfTheDay = ""),
+      (this.currentGuess = ""),
+      (this.round = 0),
+      (this.total = 0),
+      (this.firstWordPoints = 0),
+      (this.acceptableWords = new Set());
+  }
+  async qweu9rhtgi() {
+    await this.wordListLoader.loadLists(),
+      this.qwu9erhgt(),
+      this.qeujri9hgtg(),
+      this.uyq8gew0rt(),
+      this.oiyhwert();
+  }
+  qwu9erhgt() {
+    const t = new Date(),
+      e = Math.floor(
+        (t - new Date(t.getFullYear(), 0, 0)) / 1e3 / 60 / 60 / 24
+      ),
+      s = this.qiweorgt(
+        this.currentYear,
+        this.wordListLoader.qaeiurthg.length,
+        365
+      );
+    (this.wordOfTheDay = this.wordListLoader.qaeiurthg[s[e]].toUpperCase()),
+      this.wordListLoader.qaeiurthg.forEach((t) => this.acceptableWords.add(t)),
+      this.wordListLoader.qeiruyght.forEach((t) => this.acceptableWords.add(t));
+  }
+  qiweorgt(t, e, s) {
+    const r = new Set();
+    let o = 0;
+    for (; r.size < s; ) {
+      const s = Math.floor(
+        (1e5 * Math.abs(Math.sin(t + o) + Math.cos(t + o))) % e
+      );
+      r.add(s), o++;
+    }
+    return Array.from(r);
+  }
+  qeujri9hgtg() {
+    for (let t = 0; t < this.wordLength * this.maxGuesses; t++) {
+      const t = document.createElement("div");
+      t.classList.add("cell"), this.grid.appendChild(t);
+    }
+  }
+  uyq8gew0rt() {
+    this.grid.querySelectorAll(".cell").forEach((t, e) => {
+      const s = Math.floor(e / this.wordLength),
+        r = e % this.wordLength;
+      this.currentGuess.length > r && this.round === s
+        ? (t.textContent = this.currentGuess[r])
+        : s < this.round || (t.textContent = "");
+    });
+  }
+  oiyhwert() {
+    this.buttons.forEach((t) => {
+      t.addEventListener("click", () => this.bvadsf(t.textContent));
+    });
+  }
+  bvadsf(t) {
+    if ("<<<" === t) this.currentGuess = this.currentGuess.slice(0, -1);
+    else if ("Enter" === t) {
+      if (this.currentGuess.length === this.wordLength)
+        if (this.acceptableWords.has(this.currentGuess.toUpperCase())) {
+          if (
+            (this.w98iuhbet(),
+            0 === this.round &&
+              (this.firstWordPoints = this.calculateScore(this.currentGuess)),
+            this.currentGuess.toUpperCase() === this.wordOfTheDay)
+          )
+            return (
+              (this.total = this.firstWordPoints + this.round * this.RD),
+              alert(
+                `Congratulations! You guessed the word: ${this.wordOfTheDay}. Your total score is: ${this.total}`
+              ),
+              void (this.totalScorePointsSpan.textContent = this.total
+                .toString()
+                .padStart(1, "0"))
+            );
+          if (!(this.round < 5))
+            return (
+              (this.total = this.firstWordPoints + 60),
+              void alert(
+                `Game Over! The correct word was: ${this.wordOfTheDay}. Your total score is: ${this.total}`
+              )
+            );
+          (this.total = this.firstWordPoints + this.round * this.RD),
+            this.round++,
+            (this.currentGuess = "");
+        } else alert("Not a valid word");
+    } else
+      this.currentGuess.length < this.wordLength && (this.currentGuess += t);
+    this.uyq8gew0rt(),
+      (this.totalScorePointsSpan.textContent = this.total
+        .toString()
+        .padStart(1, "0"));
+  }
+  calculateScore(t) {
+    return t.split("").reduce((t, e) => t + (this.PD[e.toUpperCase()] || 0), 0);
+  }
+  w98iuhbet() {
+    const t = this.grid.querySelectorAll(".cell"),
+      e = this.currentGuess.split(""),
+      s = this.wordOfTheDay.split(""),
+      r = {};
+    e.forEach((e, o) => {
+      const i = this.round * this.wordLength + o,
+        a = t[i];
+      e === s[o] &&
+        ((a.style.backgroundColor = "green"),
+        (s[o] = null),
+        (r[e.toUpperCase()] = "green"));
+    }),
+      e.forEach((e, o) => {
+        const i = this.round * this.wordLength + o,
+          a = t[i];
+        s.includes(e) && "green" !== a.style.backgroundColor
+          ? ((a.style.backgroundColor = "#dac316"),
+            (s[s.indexOf(e)] = null),
+            r[e.toUpperCase()] || (r[e.toUpperCase()] = "#dac316"))
+          : "green" !== a.style.backgroundColor &&
+            "#dac316" !== a.style.backgroundColor &&
+            ((a.style.backgroundColor = "#636363"),
+            r[e.toUpperCase()] || (r[e.toUpperCase()] = "#636363"));
+      }),
+      this.tcpx(r);
+  }
+  tcpx(t) {
+    this.buttons.forEach((e) => {
+      const s = e.textContent.toUpperCase();
+      t[s] && (e.style.backgroundColor = t[s]);
+    });
+  }
+}
